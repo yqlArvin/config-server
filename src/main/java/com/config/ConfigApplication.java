98e59e835f329485d3afafbb799dbfcde3630148
yqlArvin <yql825018582@163.com>
2020-12-18 15:28:09 +0800

    配置调整

--- src/main/java/com/config/ConfigApplication.java
+++ src/main/java/com/config/ConfigApplication.java
@@ -2,11 +2,12 @@ package com.config;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 import org.springframework.cloud.config.server.EnableConfigServer;
-import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 
 @EnableConfigServer  // 添加注解 @EnableConfigServer
-@EnableEurekaClient  // 启用 eureka client 相关默认配置，在 Edgware 以后的版本该注解可以省略
+@EnableDiscoveryClient
+// @EnableEurekaClient 启用 eureka client 相关默认配置，在 Edgware 以后的版本该注解可以省略 只适用于Eureka作为注册中心，@EnableDiscoveryClient 可以是其他注册中心。
 @SpringBootApplication
 public class ConfigApplication {
 
