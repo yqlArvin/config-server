39070d0b7c52512bd71230ca0c7a26b130d53a92
yqlArvin <yql825018582@163.com>
2020-12-18 15:48:11 +0800

    配置信息客户端

--- src/main/java/com/config/ConfigApplication.java
+++ src/main/java/com/config/ConfigApplication.java
@@ -3,11 +3,8 @@ package com.config;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
-import org.springframework.cloud.config.server.EnableConfigServer;
 
-@EnableConfigServer  // 添加注解 @EnableConfigServer
 @EnableDiscoveryClient
-// @EnableEurekaClient 启用 eureka client 相关默认配置，在 Edgware 以后的版本该注解可以省略 只适用于Eureka作为注册中心，@EnableDiscoveryClient 可以是其他注册中心。
 @SpringBootApplication
 public class ConfigApplication {
 
